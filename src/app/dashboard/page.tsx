d0ff335f1323940204873962aff1eaf25564ffd1
Charless Wesley Arya <wesleyarya40@gmail.com>
2023-05-08 23:40:29 +0530

    this is after writing the logo and adding google svg

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/ui/Button'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button variant='ghost'>Hello</Button>
+}
+
+export default page
